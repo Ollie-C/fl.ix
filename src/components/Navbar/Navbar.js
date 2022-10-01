6e6f810d42d556d832c5f2481b0ccc888071f13d
Ollie Cross <iio.olivercross@gmail.com>
2022-10-01 15:25:45 +0100

    Created navbar structure and styling for mobile view

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import logo from "../../assets/Logo/BrainFlix-logo.svg";
+//import "../../styles/styles.scss";
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <a className="nav__logo-container">
+          <img
+            src={logo}
+            alt="egeqgwegewg"
+            className="nav__logo global__logo"
+          />
+        </a>
+        <input
+          className="nav__search global__input"
+          placeholder="Search"
+        ></input>
+        <div className="nav__avatar global__avatar global__avatar--photo"></div>
+        <button className="nav__button global__button">UPLOAD</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
