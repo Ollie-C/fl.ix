70f225789b4b5794be6c45348b8df5de0797bdcd
Ollie Cross <iio.olivercross@gmail.com>
2022-10-02 13:38:36 +0100

    Fixed resizing issues and added icons for navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import React from "react";
 import logo from "../../assets/Logo/BrainFlix-logo.svg";
+import searchIcon from "../../assets/Icons/search.svg";
+import uploadIcon from "../../assets/Icons/upload.svg";
 import "./navbar.scss";
 
 const Navbar = () => {
@@ -14,22 +16,33 @@ const Navbar = () => {
   return (
     <header className="header">
       <nav className="nav">
-        <a className="nav__logo-container">
-          <img
-            src={logo}
-            alt="egeqgwegewg"
-            className="nav__logo global__logo"
-          />
-        </a>
-        <input
-          className={`nav__search global__input ${
-            isActive ? "global__input--active" : null
-          }`}
-          onClick={toggleClass}
-          placeholder="Search"
-        ></input>
-        <div className="nav__avatar global__avatar global__avatar--photo"></div>
-        <button className="nav__button global__button">UPLOAD</button>
+        <div className="nav__logo-wrapper">
+          <a href="">
+            <img src={logo} alt="brainflix-logo" className="global__logo" />
+          </a>
+        </div>
+
+        <div className="nav__container">
+          <div className="nav__search-wrapper">
+            <img className="nav__icon" src={searchIcon} alt="search-icon" />
+            <input
+              className={`nav__search global__input ${
+                isActive ? "global__input--active" : null
+              }`}
+              onClick={toggleClass}
+              placeholder="Search"
+            ></input>
+          </div>
+          <div className="nav__avatar global__avatar global__avatar--photo"></div>
+          <div className="nav__button-wrapper">
+            <img
+              className="nav__icon nav__icon--upload"
+              src={uploadIcon}
+              alt="upload-icon"
+            />
+            <button className="global__button">UPLOAD</button>
+          </div>
+        </div>
       </nav>
     </header>
   );
