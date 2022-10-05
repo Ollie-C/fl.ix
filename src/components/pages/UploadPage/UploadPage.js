c9e3c2daf496763192340a19f2aead9d4faf22c6
Ollie Cross <iio.olivercross@gmail.com>
2022-10-05 12:11:26 +0100

    built out basic styling and layout for upload page

--- src/components/pages/UploadPage/UploadPage.js
+++ src/components/pages/UploadPage/UploadPage.js
@@ -1,29 +1,58 @@
 import uploadIcon from "../../../assets/Icons/upload.svg";
+import "./uploadpage.scss";
+import thumbnail from "../../../assets/Images/Upload-video-preview.jpg";
 
 const UploadPage = () => {
+  const [title, setTitle] = useState("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
   return (
     <>
-      <h1>Upload Video</h1>
-      <form className="comments__form">
-        <label htmlFor="">VIDEO THUMBNAIL</label>
-        <img src="" alt="img" srcset="" />
-        <label htmlFor="">TITLE YOUR VIDEO</label>
-        <input type="text" className="global__input" />
-        <label className="" htmlFor="">
-          ADD A VIDEO DESCRIPTION
-        </label>
-        <textarea
-          className="global__input"
-          name="addcomment"
-          id="addcomment"
-          placeholder="Add a new comment"
-        ></textarea>
-
-        <div className="comments__button-wrapper">
-          <img className="comments__icon" src={uploadIcon} alt="comment-icon" />
-          <button className="comments__button global__button">PUBLISH</button>
+      <article className="upload">
+        <h1 className="upload__header">Upload Video</h1>
+        <form className="upload__form" onSubmit={submitHandler}>
+          <div className="upload__container upload__container--top">
+            <label className="upload__label" htmlFor="thumbnail">
+              VIDEO THUMBNAIL
+            </label>
+            <img
+              src={thumbnail}
+              alt="img"
+              className="upload__thumbnail"
+              name="thumbnail"
+            />
+          </div>
+          <div className="upload__container upload__container--middle">
+            <label className="upload__label" htmlFor="addtitle">
+              TITLE YOUR VIDEO
+            </label>
+            <input
+              type="text"
+              className="upload__title global__input"
+              name="addtitle"
+              placeholder="Add a title to your video"
+            />
+            <label className="upload__label" htmlFor="">
+              ADD A VIDEO DESCRIPTION
+            </label>
+            <textarea
+              className="upload__description global__input"
+              name="adddescription"
+              id="adddescription"
+              placeholder="Add a description to your video"
+            ></textarea>
+          </div>
+        </form>
+        <div className="upload__container upload__container--bottom">
+          <div className="upload__button-wrapper">
+            <img className="upload__icon" src={uploadIcon} alt="upload-icon" />
+            <button className="upload__button global__button">PUBLISH</button>
+          </div>
+          <p className="upload__cancel">CANCEL</p>
         </div>
-      </form>
+      </article>
     </>
   );
 };
